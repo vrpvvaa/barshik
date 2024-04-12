4e3573b4157becf01eb41b168c7cb923dcc3d230
vrpvvaa <148848308+vrpvvaa@users.noreply.github.com>
2024-04-13 04:10:38 +0500

    шапка и подвал
    удобно подключаем их теперь

---
+++ connect/footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="links">
+        <h3>Контакты</h3>
+        <ul>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+        </ul>
+    </div>
+
+    <div class="logo-cont">
+        <img class="logo" src="../images/logo.png" alt="Лого">
+        <p>FIZZ (c) 2024 All rights reserved.</p>
+    </div>
+    <div class="links">
+        <h3>Соц. сети</h3>
+        <ul>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">О нас</a></li>
+        </ul>
+    </div>
+</footer>
---
+++ connect/header.php
@@ -0,0 +1,6 @@
+<nav>
+    <a id="logo-link" href="../index.php"><img class="logo" src="../images/logo.png" alt="Лого"></a>
+    <div class="reg-auth-links">
+    <a href="../reg-auth/authorization.php"><img id="login" src="../images/login.png" alt="Войти"></a>
+    </div>
+</nav>
